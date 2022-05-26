2b9a750b678dd9029056c1426b6f0ee4b6f35f2d
Almeidiano <samuel10ap@gmail.com>
2022-05-26 12:56:34 -0300

    Buttons css changes, changing png to webp, and putting all characters percentages on result

--- assets/js/result.js
+++ assets/js/result.js
@@ -2,11 +2,11 @@ const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
 const characters = [
-	{id: 1, characterNick: "madger", characterName: "Madger Yasáshi", characterImg: 'assets/img/characters/madger.png', characterAttr: "extro", characterDesc: 'Você é uma pessoa bastante <span style="color:lightgreen;">extrovertida</span>, você ama aventuras e é bastante emotivo(a). Aproximando-se bastante da personalidade do Madger.'},
-	{id: 2, characterNick: "singer", characterName: "Singer Faksumi", characterImg: 'assets/img/characters/singer.png', characterAttr: "intro", characterDesc: 'Você é uma pessoa que possui características de <span style="color:lightblue;">introversão</span>. Além disso, observador e pensativo. Aproximando-se bastante da personalidade do Singer.'},
-	{id: 3, characterNick: "san", characterName: "San Majutsu-shi", characterImg: 'assets/img/characters/san.png', characterAttr: "intellectual", characterDesc: 'Você é uma pessoa com muito foco no <span style="color:orange;">intelectual</span>, você é bastante racional e até peca na questão física. Aproximando-se bastante da personalidade do San.'},
-	{id: 4, characterNick: "aika", characterName: "Aika'Nu", characterImg: 'assets/img/characters/aika.png', characterAttr: "charisma", characterDesc: 'Você é uma pessoa <span style="color:pink;">carismática</span>, você se preocupa bastante com sua família e amigos. Ademais, você possui um bom juízo. Aproximando-se bastante da personalidade da Aika.'},
-	{id: 5, characterNick: "parasitum", characterName: "Parasita Demoniaco", characterImg: 'assets/img/characters/unum-parasitum.png', characterAttr: "parasitum", characterDesc: 'você é uma pessoa que adora ver sangue HAHAHAHAHAHAHAHA, você adora invadir sistemas e quebrar regras, e quanto mais gente sofre no processo, melhor, afinal, nao tem nada mais prazeroso do que fazer todos sofrerem com o que você sofreu HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA'}
+	{id: 1, characterNick: "madger", characterName: "Madger Yasáshi", characterImg: 'assets/img/characters/madger.webp', characterAttr: "extro", characterDesc: 'Você é uma pessoa bastante <span style="color:lightgreen;">extrovertida</span>, você ama aventuras e é bastante emotivo(a). Aproximando-se bastante da personalidade do Madger.'},
+	{id: 2, characterNick: "singer", characterName: "Singer Faksumi", characterImg: 'assets/img/characters/singer.webp', characterAttr: "intro", characterDesc: 'Você é uma pessoa que possui características de <span style="color:lightblue;">introversão</span>. Além disso, observador e pensativo. Aproximando-se bastante da personalidade do Singer.'},
+	{id: 3, characterNick: "san", characterName: "San Majutsu-shi", characterImg: 'assets/img/characters/san.webp', characterAttr: "intellectual", characterDesc: 'Você é uma pessoa com muito foco no <span style="color:orange;">intelectual</span>, você é bastante racional e até peca na questão física. Aproximando-se bastante da personalidade do San.'},
+	{id: 4, characterNick: "aika", characterName: "Aika'Nu", characterImg: 'assets/img/characters/aika.webp', characterAttr: "charisma", characterDesc: 'Você é uma pessoa <span style="color:pink;">carismática</span>, você se preocupa bastante com sua família e amigos. Ademais, você possui um bom juízo. Aproximando-se bastante da personalidade da Aika.'},
+	{id: 5, characterNick: "parasitum", characterName: "Parasita Demoniaco", characterImg: 'assets/img/characters/unum-parasitum.webp', characterAttr: "parasitum", characterDesc: 'você é uma pessoa que adora ver sangue HAHAHAHAHAHAHAHA, você adora invadir sistemas e quebrar regras, e quanto mais gente sofre no processo, melhor, afinal, nao tem nada mais prazeroso do que fazer todos sofrerem com o que você sofreu HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA'}
 ];
 
 const charResult = localStorage.getItem("charResult");
@@ -52,22 +52,18 @@ function applyCharResult() {
 			c(".character h1").innerHTML = finalCharacter.characterName;
 			switch(character.characterNick) {
 				case "madger":
-					c(".madgerChar").remove();
 					c(".char-thumb").classList.add("madger");
 					c(".character h1").style.color = "lightgreen"
 				break;
 				case "singer":
-					c(".singerChar").remove();
 					c(".char-thumb").classList.add("singer");
 					c(".character h1").style.color = "lightblue"
 				break;
 				case "san":
-					c(".sanChar").remove();
 					c(".char-thumb").classList.add("san");
 					c(".character h1").style.color = "orange"
 				break;
 				case "aika":
-					c(".aikaChar").remove();
 					c(".char-thumb").classList.add("aika");
 					c(".character h1").style.color = "pink"
 				break;
@@ -84,32 +80,25 @@ function applyCharResult() {
 function applyCharPercentage(){
 	const allCharacters = JSON.parse(localStorage.getItem("allCharacters"));
 	console.log(allCharacters);
-	function findAndApply(character, characteristic){
-		cs(".character").forEach(char => {
-			if(char.classList.contains(character)){
-				c(`.${CSS.escape(character)} .percentage`).innerText = characteristic;
-			}
-		})
-	}
 
 	Object.keys(allCharacters).forEach(char => {
 		switch(char){
 			case 'intro':
-				findAndApply('singerChar', `${allCharacters.intro}%`);
+				c('.singerChar .percentage').innerText = `${allCharacters.intro}%`;
 			break;
 			case 'intellectual':
-				findAndApply('sanChar', `${allCharacters.intellectual}%`);
+				c('.sanChar .percentage').innerText = `${allCharacters.intellectual}%`;
 			break;
 			case 'extro':
-				findAndApply('madgerChar', `${allCharacters.extro}%`);
+				c('.madgerChar .percentage').innerText = `${allCharacters.extro}%`;
 			break;
 			case 'charisma':
-				findAndApply('aikaChar', `${allCharacters.charisma}%`);
+				c('.aikaChar .percentage').innerText = `${allCharacters.charisma}%`;
 			break;
 		}
 	})
 
-	localStorage.clear();
+	// localStorage.clear();
 }
 
 export {c, characters, charResult, checkCharResult, applyCharResult, applyCharPercentage}
